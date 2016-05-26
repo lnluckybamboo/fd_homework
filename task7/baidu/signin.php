c5d0a7dbf45ba7affecfef2c2951359450646227
lnluckybamboo <lnluckybamboo@163.com>
2016-05-26 14:42:48 +0800

    delete 7

--- task7/baidu/signin.php
+++
@@ -1,3 +0,0 @@
-<?php 
-echo json_encode(Array('username'=>$_GET['username'], 'password'=>$_GET['password']));
-?>
--- task7/jikexueyuan/js/strategy.js
+++
@@ -1,9 +0,0 @@
-define(function(require, exports, module) {
-    var swiperSea = require('swiper');
-    new swiperSea('.strategy-box', {
-        loop: true,
-        nextButton: '#banner-right3',
-        prevButton: '#banner-left3',
-        slidesPerView: 6
-    });
-});
